fc3af0d19160737e07b77bce60c884f6802109e5
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-03-07 22:49:09 -0800

    add basic layout for ToDo page

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,7 +1,80 @@
 function ToDo() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">ToDo page</h1>
+    <div className="border-2 border-sky-500 min-h-screen">
+      <div className="w-3/4  border-2 border-yellow-500 mx-auto mt-5 flex flex-col flex-nowrap">
+        <div className="w-full border-2 border-green-500 mx-auto mt-5 flex-auto">
+          <h1 class="text-3xl font-bold underline">Tasks</h1>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 1</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 2</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+        <div className="w-full border-2 border-green-500 mx-auto mt-2 flex-auto">
+          <p class="text-2xl">Task 3</p>
+        </div>
+      </div>
+      <div className="w-full h-28 border-2 border-black mx-auto fixed bottom-0 text-right">
+        <div className="w-16 h-16 border-2 border-yellow-800 mr-6 ml-auto mt-4"></div>
+      </div>
     </div>
   );
 }
