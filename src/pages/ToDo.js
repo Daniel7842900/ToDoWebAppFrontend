d549294a5f1a408b56f7af1ad1f978cc5bd641a7
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-03-13 22:45:49 -0700

    add handlers to open and close modal

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,14 +1,24 @@
 import TaskList from "../features/todo/components/TaskList";
 import Modal from "../layouts/Modal";
+import { useState } from "react";
 
 function ToDo() {
+  const [show, setShow] = useState(false);
+
+  const handleOpenModal = () => {
+    setShow(true);
+  };
+
   return (
     <div className="border-2 border-sky-500 min-h-screen">
-      <Modal />
+      {show ? <Modal show={show} setShow={setShow} /> : null}
       <TaskList />
       {/* Task Add Button - Mobile */}
       <div className="w-full h-28 border-2 border-black mx-auto fixed bottom-0 text-right lg:hidden xl:hidden 2xl:hidden">
-        <div className="w-16 h-16 border-2 border-yellow-800 mr-6 ml-auto mt-4"></div>
+        <div
+          className="w-16 h-16 border-2 border-yellow-800 mr-6 ml-auto mt-4"
+          onClick={handleOpenModal}
+        ></div>
       </div>
     </div>
   );
